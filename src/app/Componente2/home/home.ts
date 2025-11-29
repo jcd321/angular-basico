08dc5253bf3c9469adea5c209ceca67da8820f77
Jonathan Cuello <81935131+jcd321@users.noreply.github.com>
2025-11-28 20:34:49 -0500

    update code

--- src/app/Componente2/home/home.ts
+++ src/app/Componente2/home/home.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.css',
+  imports: [],
 })
 export class Home {
+setClasses() {
+throw new Error('Method not implemented.');
+}
+message: any;
+clearAlert() {
+throw new Error('Method not implemented.');
+}
+showAlert(arg0: string) {
+throw new Error('Method not implemented.');
+}
 
   titulo: string = 'Mi Primera App Angular';
 
@@ -15,12 +27,18 @@ export class Home {
   servicio:boolean = true;
   modoOscuro: boolean = false;
 
+  mostrarAlerta = false;
+  mensajeAlerta = 'Este es un mensaje de ejemplo del sistema.';
+  mostrarSoloPendientes = false;
+
   servicios: string[] = [
     'Servicios de actualizacion',
     'Servicios de mantenimiento',
     'Servicios de soporte tecnico',
     'Servicios de consultoria'
   ];
+  
+  type: string | undefined;
 
   saludar() {
     console.log('Botón 1 presionado');
@@ -40,4 +58,22 @@ export class Home {
     this.modoOscuro = !this.modoOscuro;
   }
 
+alternarVista() {
+this.mostrarSoloPendientes = !this.mostrarSoloPendientes;
+
+  if (this.mostrarSoloPendientes) {
+  this.mensajeAlerta = '¡Filtro activado! Mostrando solo temas pendientes.';
+  } else {
+  this.mensajeAlerta = 'Filtro desactivado. Mostrando todos los temas.';
+  }
+  this.mostrarAlerta = true;
+  }
+  abrirModal() {
+    this.mensajeAlerta = '';
+    this.mostrarAlerta = true;
+  }
+
+  cerrarAlerta() {
+    this.mostrarAlerta = false;
+  }
 }
